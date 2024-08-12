0f07ab3c8843496a4fe81e0f7e2cdc45948c83b9
El_monoy <yairalejandroosorioalzate38@gmail.com>
2024-08-11 21:06:13 -0500

    TRABAJO_GRTP

---
+++ app/src/main/java/com/cursokotlin/routemapexample/MainActivity.kt
@@ -0,0 +1,126 @@
+package com.cursokotlin.routemapexample
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Polyline
+import com.google.android.gms.maps.model.PolylineOptions
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var map: GoogleMap
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var btnCalculate: Button
+
+    private var start: String = ""
+    private var end: String = ""
+
+    var poly: Polyline? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnCalculate = findViewById(R.id.btnCalculateRoute)
+        btnCalculate.setOnClickListener {
+            start = ""
+            end = ""
+            poly?.remove()
+            poly = null
+            Toast.makeText(this, "Selecciona punto de origen y final", Toast.LENGTH_SHORT).show()
+            if (::map.isInitialized) {
+                map.setOnMapClickListener {
+                    if (start.isEmpty()) {
+                        start = "${it.longitude},${it.latitude}"
+                    } else if (end.isEmpty()) {
+                        end = "${it.longitude},${it.latitude}"
+                        createRoute()
+                    }
+                }
+            }
+        }
+
+        // Inicializar el cliente de ubicación
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(map: GoogleMap) {
+        this.map = map
+        enableMyLocation()
+    }
+
+    private fun enableMyLocation() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+            == PackageManager.PERMISSION_GRANTED) {
+            map.isMyLocationEnabled = true
+
+            // Obtener la última ubicación conocida y mover la cámara a esa ubicación
+            fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
+                location?.let {
+                    val userLocation = LatLng(it.latitude, it.longitude)
+                    map.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation, 15f))
+                }
+            }
+        } else {
+            // Solicitar permisos si no se han concedido
+            ActivityCompat.requestPermissions(this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                LOCATION_PERMISSION_REQUEST_CODE)
+        }
+    }
+
+    private fun createRoute() {
+        CoroutineScope(Dispatchers.IO).launch {
+            val call = getRetrofit().create(ApiService::class.java)
+                .getRoute("5b3ce3597851110001cf6248abe81b05d8314a5ba17589eb9f295695", start, end)
+            if (call.isSuccessful) {
+                drawRoute(call.body())
+            } else {
+                Log.i("aris", "KO")
+            }
+        }
+    }
+
+    private fun drawRoute(routeResponse: RouteResponse?) {
+        val polyLineOptions = PolylineOptions()
+        routeResponse?.features?.first()?.geometry?.coordinates?.forEach {
+            polyLineOptions.add(LatLng(it[1], it[0]))
+        }
+        runOnUiThread {
+            poly = map.addPolyline(polyLineOptions)
+        }
+    }
+
+    private fun getRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://api.openrouteservice.org/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    companion object {
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1
+    }
+}
